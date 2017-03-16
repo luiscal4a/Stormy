e1c146af237f946df18a21ad564bb7fe9ab2e1f6
Luis de la Cal <luiscal4a@gmail.com>
2017-03-16 17:04:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/stormy/CurrentWeather.java
@@ -0,0 +1,122 @@
+package com.example.android.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Luis on 2017-03-14.
+ */
+
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipiChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public int getIconId(){
+        int iconId = R.mipmap.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime()*1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public int getPrecipiChance() {
+        double preciPercentage = mPrecipiChance * 100;
+        return (int) Math.round(preciPercentage);
+    }
+
+    public void setPrecipiChance(double precipiChance) {
+        mPrecipiChance = precipiChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
